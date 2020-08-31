d69dba2fa8e54d023826750a98980aa055bc86a3
beverlywarren <hnrowei@gmail.com>
2020-08-31 19:59:42 +0800

    rowei

---
+++ setup_test.go
@@ -0,0 +1,29 @@
+package admin_test
+
+import (
+	"net/http"
+	"net/http/httptest"
+
+	"github.com/conku/admin"
+	. "github.com/conku/admin/tests/dummy"
+	"github.com/jinzhu/gorm"
+)
+
+var (
+	server *httptest.Server
+	db     *gorm.DB
+	Admin  *admin.Admin
+)
+
+func init() {
+	var mux http.Handler
+	mux, Admin, db = NewTestHandler()
+	server = httptest.NewServer(mux)
+}
+
+func NewTestHandler() (h http.Handler, adm *admin.Admin, d *gorm.DB) {
+	adm = NewDummyAdmin()
+	d = adm.DB
+	h = adm.NewServeMux("/admin")
+	return
+}
